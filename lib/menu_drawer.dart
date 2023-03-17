30a1e1ee2bd51eb0df7d7892a530a641c6731bba
gChagasGit <chagasgustavo10@gmail.com>
2023-03-16 22:59:28 -0300

    Animação slider do SwitchListTile

--- lib/menu_drawer.dart
+++ lib/menu_drawer.dart
@@ -10,23 +10,32 @@ class MenuDrawer extends StatefulWidget {
 }
 
 class _MenuDrawerState extends State<MenuDrawer> {
+  late final ValueNotifier<bool> _darkThemeNotifier;
+
+  @override
+  void initState() {
+    super.initState();
+    _darkThemeNotifier = ValueNotifier(ThemeController.instance.isDarkTheme);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: Column(
         children: [
           Expanded(
-              child: Column(
-            children: const [
-              UserAccountsDrawerHeader(
-                accountName: Text("Nome"),
-                accountEmail: Text("Email@email.com"),
-                currentAccountPicture: CircleAvatar(
-                  child: Icon(Icons.person),
+            child: Column(
+              children: const [
+                UserAccountsDrawerHeader(
+                  accountName: Text("Nome"),
+                  accountEmail: Text("Email@email.com"),
+                  currentAccountPicture: CircleAvatar(
+                    child: Icon(Icons.person),
+                  ),
                 ),
-              )
-            ],
-          )),
+              ],
+            ),
+          ),
           Align(
             alignment: FractionalOffset.bottomCenter,
             child: Column(
@@ -35,12 +44,17 @@ class _MenuDrawerState extends State<MenuDrawer> {
                 const ListTile(
                     leading: Icon(Icons.settings),
                     title: Text('Configurações')),
-                SwitchListTile(
-                  title: const Text('Tema escuro'),
-                  value: ThemeController.instance.isDarkTheme,
-                  controlAffinity: ListTileControlAffinity.leading,
-                  onChanged: (bool value) {
-                    ThemeController.instance.changeTheme();
+                ValueListenableBuilder<bool>(
+                  valueListenable: _darkThemeNotifier,
+                  builder: (BuildContext context, bool value, Widget? child) {
+                    return SwitchListTile(
+                      title: const Text('Tema escuro'),
+                      value: value,
+                      onChanged: (bool newValue) {
+                        _darkThemeNotifier.value = newValue;
+                        ThemeController.instance.changeTheme();
+                      },
+                    );
                   },
                 ),
               ],
