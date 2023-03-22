4006130122be08372db5e86d71759dd7bb480244
gChagasGit <chagasgustavo10@gmail.com>
2023-03-22 18:18:55 -0300

    Adicionando models

---
+++ lib/models/log.dart
@@ -0,0 +1,13 @@
+class Log{
+  
+  String idLog;
+  String device_idDevice;
+  DateTime time;
+  String type;
+
+  Log(
+      {required this.idLog,
+      required this.device_idDevice,
+      required this.time,
+      required this.type});
+}
---
+++ lib/models/users.dart
@@ -0,0 +1,9 @@
+class Usuario {
+  
+  String id;
+  String? name;
+  String email;
+  String? password;
+
+  Usuario({required this.id, this.name = '', required this.email, this.password});
+}
