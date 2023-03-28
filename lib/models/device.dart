bd59d12f3bb0bb54ddad6c3178c0e0f3c9aa32aa
gChagasGit <chagasgustavo10@gmail.com>
2023-03-27 22:02:15 -0300

    Adição do RealTime e page de devices

--- lib/models/device.dart
+++ lib/models/device.dart
@@ -1,13 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class device {
-  String idDevice;
+class Device {
+  // String idDevice;
   String macAddress;
   bool active;
   bool triggered;
 
-  device({
-    required this.idDevice,
+  Device({
+    // required this.idDevice,
     required this.macAddress,
     required this.active,
     required this.triggered,
@@ -15,27 +15,35 @@ class device {
 
   Map<String, dynamic> toMap() {
     return {
-      'idDevice': idDevice,
+      // 'idDevice': idDevice,
       'macAddress': macAddress,
       'active': active,
       'triggered': triggered,
     };
   }
 
-  device.fromMap(Map<String, dynamic> map)
-      : idDevice = map['idDevice'],
+  Device.fromMap(Map<String, dynamic> map)
+      // : idDevice = map['idDevice'],
+      :
         macAddress = map['macAddress'],
         active = map['active'],
         triggered = map['triggered'];
 
   // Obtendo um objeto Device a partir de um documento
-  static Future<device?> getDeviceFromDocument(
+  static Future<Device?> getDeviceFromDocument(
       QueryDocumentSnapshot<Object?> document) async {
     Map<String, dynamic>? data = document.data() as Map<String, dynamic>?;
     if (data == null) {
       return null;
     }
-    device _device = device.fromMap(data);
+    Device _device = Device.fromMap(data);
     return _device;
   }
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return super.toString();
+  }
+
 }
