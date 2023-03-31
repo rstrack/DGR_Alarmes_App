95a743da44682889d47d60fff6d829d6b8981bd3
Rafael Strack <strackinado@gmail.com>
2023-03-31 01:10:51 -0300

    correção em bug do git no arquivo user.dart

---
+++ lib/models/user.dart
@@ -0,0 +1,26 @@
+class User {
+  String id;
+  String name;
+  String email;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.email,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json.keys.first,
+      name: json['name'],
+      email: json['email'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'email': email,
+    };
+  }
+}
