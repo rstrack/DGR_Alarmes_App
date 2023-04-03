97be0fef564e346da144f4712da7152169a5beea
Rafael Strack <strackinado@gmail.com>
2023-04-03 17:55:26 -0300

    função getLogs: adição de exclusão de logs antigos quando existirem mais de 30 logs

--- lib/controller/log_controller.dart
+++ lib/controller/log_controller.dart
@@ -3,20 +3,38 @@ import 'package:firebase_database/firebase_database.dart';
 
 class LogController {
   static final DatabaseReference _ref = FirebaseDatabase.instance.ref();
-  getLogs(String macAddress, {int limit = 10}) async {
-    var event = await _ref
+
+  ///recupera os 30 últimos logs e, se existir mais de 30 logs, exclui os mais antigos
+  getLogs(String macAddress, {int limit = 30}) async {
+    DatabaseEvent event = await _ref
         .child('log/$macAddress')
         .orderByChild('time')
         .limitToLast(limit)
         .once();
-    final data = event.snapshot.value;
+
+    if (event.snapshot.value == null) return;
+
     List<Log> logs = [];
-    if (data != null) {
-      (data as Map).forEach((key, value) {
-        logs.add(Log.fromJson(value));
-      });
-      return logs;
+    final data = event.snapshot.value as Map;
+
+    if (data.length == 30) {
+      //exclusão de logs excedentes
+      DatabaseEvent surplusEvent = await _ref
+          .child('log/$macAddress')
+          .orderByChild('time')
+          .endBefore(data.entries.first.value['time'] - 1,
+              key: 'time') //endBefore está bugado, por isso o uso do -1
+          .once();
+      if (surplusEvent.snapshot.value != null) {
+        (surplusEvent.snapshot.value as Map).forEach((key, value) async {
+          await _ref.child('log/$macAddress/$key').remove();
+        });
+      }
     }
+    data.forEach((key, value) {
+      logs.add(Log.fromJson(value));
+    });
+    return logs.reversed.toList();
   }
 
   static LogController instance = LogController();
