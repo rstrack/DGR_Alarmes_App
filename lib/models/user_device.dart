048bffefcbacfc9e7b3ffb8a3c664faab6cdd6f5
gChagasGit <chagasgustavo10@gmail.com>
2023-04-05 10:17:33 -0300

    alterações gerais

---
+++ lib/models/user_device.dart
@@ -0,0 +1,33 @@
+class UserDevice {
+  String idUser;
+  String idDevice;
+  String nickname;
+
+  UserDevice({
+    required this.idUser,
+    required this.idDevice,
+    required this.nickname,
+  });
+
+  factory UserDevice.fromJson(Map<String, dynamic> json) {
+    return UserDevice(
+      idUser: json['idUser'],
+      idDevice: json['idDevice'],
+      nickname: json['nickname'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'idUser': idUser,
+      'idDevice': idDevice,
+      'nickname': nickname,
+    };
+  }
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return "UserDevice | idUser: $idUser | idDevice: $idDevice | nickname: $nickname";
+  }
+}
